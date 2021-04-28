ec4196560c1ad86a04cf2661e33e83f69c40c0e2
Juan Daniel Alvarez <76133285+DanielMonsivais@users.noreply.github.com>
2021-04-28 00:50:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,50 +12,67 @@ colors = {
     'text': "#000000"
 }
 
-# Datos de pandas
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-df_2 = px.data.election()
 
-# Graficas de plotly
-fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='stack')
-fig_2 = px.scatter_3d(df_2, x="Joly", y="Coderre", z="Bergeron", color="winner", size="total", hover_name="district",
-                      symbol="result", color_discrete_map = {"Joly": "blue", "Bergeron": "green", "Coderre":"red"})
 
 ##############################################################################
-
+#MAPA Porcentajes de Prevalencia de Enfermedades  
 import folium
 import geopandas as gpd 
 
-state_data=pd.read_csv('https://raw.githubusercontent.com/DanielMonsivais/PRUEBAM/main/dataset/Tasa%20de%20Obesidad%202018.csv')
-state_data=state_data.replace({"Coahuila de Zaragoza": "Coahuila","Michoacán de Ocampo":"Michoacán","Veracruz de Ignacio de la Llave":"Veracruz"})
-state_geo ='https://raw.githubusercontent.com/DanielMonsivais/PRUEBAM/main/mexico.json'
-
+state_geo ='https://raw.githubusercontent.com/angyf/proyecto/main/mexico.json'
 data_geo = gpd.read_file(state_geo)
-data_geo = data_geo.rename(columns={'name':'Entidad'})
-geo=data_geo.merge(state_data,on="Entidad")
 
-#Ubicación de México en el mapa
-m = folium.Map(location=[24, -102], zoom_start=5, width=1332, height=523,tiles='Stamen Watercolor')
-#Tiles [OpenStreetMap, Mapbox Bright, Mapbox Control Room, Stamen Terrain, Stamen Toner, Stamen Watercolor, CartoDB positron, CartoDB dark_matter]
+df=pd.read_csv('https://raw.githubusercontent.com/angyf/proyecto/main/Datos-porcentaje-poblacion-mayor-20.csv',index_col=0)
+df=df.replace({"Coahuila de Zaragoza": "Coahuila","Michoacán de Ocampo":"Michoacán","Veracruz de Ignacio de la Llave":"Veracruz", "Querétaro de Arteaga":"Querétaro"})
+geo_porc=data_geo.merge(df,on="Entidad")
 
+#Se crea el mapa, añadiendo las coordenadas de la ubicación de México
+m = folium.Map(location=[24, -102], zoom_start=5, width='70%', height='100%',tiles='Stamen Watercolor')
 
-#Mapa Coroplético de la tasa de obesidad 
+#Se crean los Mapas Coropléticos, manejando el geojson de forma separada
 
+#Layer o capa que muestra los porcentajes de Obesidad
 folium.Choropleth(
     geo_data=state_geo,
-    name='Tasa de Obesidad',
-    data=state_data,
-    columns=['Entidad', 'Tasa de obesidad'],
+    name='Obesidad',
+    data=df,
+    columns=['Entidad', 'Obesidad'],
+    key_on='feature.properties.name',
+    fill_color='YlGn',
+    fill_opacity=1,
+    line_opacity=0.4,
+    legend_name='Obesidad'
+).add_to(m)
+
+#Layer o capa que muestra los porcentajes de Hipertensión
+folium.Choropleth(
+    geo_data=state_geo,
+    name='Hipertensión',
+    data=df,
+    columns=['Entidad', 'Diagnóstico previo de hipertensión'],
     key_on='feature.properties.name',
     fill_color='YlOrRd',
-    fill_opacity=0.7,
+    fill_opacity=1,
     line_opacity=0.4,
-    legend_name='Tasa de Obesidad'
+    legend_name='Hipertensión'
 ).add_to(m)
+
+#Layer o capa que muestra los porcentajes de Diabetes
+folium.Choropleth(
+    geo_data=state_geo,
+    name='Diabetes',
+    data=df,
+    columns=['Entidad', 'Diagnóstico previo de diabetes'],
+    key_on='feature.properties.name',
+    fill_color='RdPu',
+    fill_opacity=1,
+    line_opacity=0.4,
+    legend_name='Diabetes'
+).add_to(m)
+
+
+#Se crea la función con el dataframe que contiene todos los datos
+#Se selecciona la información a mostrar y las etiquetas, así como su estilo
 
 style_function = lambda x: {'fillColor': '#ffffff', 
                             'color':'#000000', 
@@ -66,31 +83,111 @@ highlight_function = lambda x: {'fillColor': '#000000',
                                 'fillOpacity': 0.7, 
                                 'weight': 0.1}
 NIL = folium.features.GeoJson(
-    geo,
-    style_function=style_function, 
+    geo_porc,
+    style_function=style_function,
+    overlay=True, 
+    show=True,
     control=False,
     highlight_function=highlight_function, 
     tooltip=folium.features.GeoJsonTooltip(
-        fields=['Entidad','Tasa de obesidad'],
-        aliases=['Estado: ','Tasa de obesidad: '],
+        fields=['Entidad','Obesidad','Diagnóstico previo de hipertensión','Diagnóstico previo de diabetes'],
+        aliases=['Entidad: ','Obesidad: ','Hipertensión: ','Diabetes: '],
         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
     )
 )
 m.add_child(NIL)
 m.keep_in_front(NIL)
-folium.LayerControl().add_to(m)
 
-loc = 'Tasa de Obesidad'
+#Se añade un título
+loc = 'Porcentajes de Prevalencia de enfermedades 2018.'
 title_html = '''
-             <h3 align="center" style="font-size:16px"><b>{}</b></h3>
-             '''.format(loc) 
-
+            <h3 style="font-size:20px"><b>{}</b></h3><br>
+             '''.format(loc)
 m.get_root().html.add_child(folium.Element(title_html))
 
-m.save('map-with-title.html')
+#Se quitan los botones de zoom para que no se empalmen con las leyendas
+m.zoom_control=False
+m.objects_to_stay_in_front
+
+#Se añade la opción para cambiar entre las capas del mapa
+folium.LayerControl().add_to(m)
+
+m.save('map-porcentajes.html')
+
 
 ##############################################################################
+#Mapa Defunciones
+data=pd.read_csv('https://raw.githubusercontent.com/angyf/proyecto/main/Tasa_Mortalidad_Cardiovascular_Serie_Historica.csv')
 
+data=data.replace({"Coahuila de Zaragoza": "Coahuila", "Distrito Federal": "Ciudad de México","Michoacán de Ocampo":"Michoacán","Veracruz Llave":"Veracruz", "Querétaro de Arteaga":"Querétaro"})
+data= data.rename(columns={'Entidad de Residencia':'Entidad'})
+data=data[data['Entidad']!="Nacional"]
+
+data_2013=data[data['Periodo']==2013]
+geo_2013=data_geo.merge(data_2013,on="Entidad")
+
+direcc=pd.read_csv('https://raw.githubusercontent.com/angyf/proyecto/main/Direcciones-coord.csv')
+
+#Se crea el mapa de igual forma al anterior
+
+m = folium.Map(location=[24, -102], zoom_start=5, width='70%', height='70%')
+
+folium.Choropleth(
+    geo_data=state_geo,
+    name='2013',
+    data=data_2013,
+    columns=['Entidad', 'Defunciones por Enfermedades Cardiovasculares'],
+    key_on='feature.properties.name',
+    fill_color='RdPu',
+    fill_opacity=1,
+    line_opacity=0.4,
+    legend_name='Defunciones por Enfermedades Cardiovasculares'
+).add_to(m)
+
+
+style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+highlight_function = lambda x: {'fillColor': '#000000', 
+                                'color':'#000000', 
+                                'fillOpacity': 0.7, 
+                                'weight': 0.1}
+NIL = folium.features.GeoJson(
+    geo_2013,
+    style_function=style_function, 
+    control=False,
+    highlight_function=highlight_function, 
+    tooltip=folium.features.GeoJsonTooltip(
+        fields=['Entidad','Poblacion Total','Defunciones por Enfermedades Cardiovasculares', 'Tasa de Mortalidad por Enfermedades Cardiovasculares'],
+        aliases=['Estado: ','Población: ','Defunciones: ','Tasa de Mortalidad: '],
+        style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+    )
+)
+m.add_child(NIL)
+m.keep_in_front(NIL)
+
+
+loc = 'Tasa de Mortalidad por Enfermedades Cardiovasculares 2013'
+title_html = '''
+             <h3 style="font-size:20px"><b>{}</b></h3><br>
+             '''.format(loc)
+m.get_root().html.add_child(folium.Element(title_html))
+
+
+folium.LayerControl().add_to(m)
+
+
+#Se añade un for que va añadiendo los marcadores, tomando las coordenadas y el nombre del dataframe
+for i in direcc.index:
+  folium.Marker(
+    location=direcc['Coordenadas'][i],
+    popup=direcc['Centro'][i],
+    ).add_to(m)
+
+m.save('defunciones.html')
+
+##############################################################################
 
 
 
@@ -111,9 +208,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     , style={
         'color': colors['text']
     }),
-    html.Iframe(id='map', srcDoc=open('map-with-title.html','r').read(),width='100%', height='523'),
-    dcc.Graph(id='example-graph', figure=fig),
-    dcc.Graph(id='example-3d', figure=fig_2)
+    html.Iframe(id='map', srcDoc=open('map-porcentajes.html','r').read(),width='100%', height='523'),
+    html.Iframe(id='map', srcDoc=open('defunciones.html','r').read(),width='100%', height='523')
 ])
 
 if __name__ == '__main__':
