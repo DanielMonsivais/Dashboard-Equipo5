d4a1955071dab2807316c8b6293fd17f94ac35b6
Juan Daniel Alvarez <76133285+DanielMonsivais@users.noreply.github.com>
2021-06-29 18:39:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'padding':
     }),
     html.Img(id = 'plot', src = 'https://raw.githubusercontent.com/DanielMonsivais/Dashboard-Equipo5/main/grafica.jpg'),
     
-    html.Iframe(width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNjhiODVkYmEtMjIzMy00MDc5LTg5ODQtNmY4MmYxZDhmMmZlIiwidCI6IjVmMjgyOTEwLTE3NmYtNDU5ZC1hYjdkLWI3NDRhYTZlZmMwNyIsImMiOjR9&pageName=ReportSection" frameborder="0" allowFullScreen="true")
+    html.Iframe(width='600',height='373',src="https://app.powerbi.com/view?r=eyJrIjoiNjhiODVkYmEtMjIzMy00MDc5LTg5ODQtNmY4MmYxZDhmMmZlIiwidCI6IjVmMjgyOTEwLTE3NmYtNDU5ZC1hYjdkLWI3NDRhYTZlZmMwNyIsImMiOjR9&pageName=ReportSection",frameborder='0',allowFullScreen='true')
 ])
 
 if __name__ == '__main__':
